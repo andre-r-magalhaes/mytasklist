20d9886ca372af30119a8e4bf39f16debbe53e42
André Ricardo <andre.r.magalhaes@gmail.com>
2015-12-16 22:13:40 -0200

    first

---
+++ br.com.mytasklist.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="br.com.mytasklist.MvcApplication" Language="C#" %>
---
+++ br.com.mytasklist.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(br.com.mytasklist.Startup))]
+namespace br.com.mytasklist
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
